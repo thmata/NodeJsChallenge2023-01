d3cc36b445ea793bb9392657def9ed3dbb13d9c0
Thiago Mata <taiagomata@gmail.com>
2023-02-04 23:19:42 -0300

    Created GET and POST

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const app = express()
+const crypto = require('crypto')
 
-app.get( "/api/products/:id" , (req, res) => {
-    const { id } = req.params
-    
-    console.log("Aqui está o ID", id)
+const database = []
+
+app.use(express.json())
+
+app.post("/tasks", (req, res) => {
+    const { title, description, } = req.body
+    const date = new Date()
+
+    const data = {
+        id: crypto.randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: (`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`),
+        updated_at: null
+    }
+
+    database.push(data)
+
+    console.log(database)
+    return res.status(200).send("Tarefa Registrada!")
+
 })
 
+app.get("/tasks", (req, res) => {
+    return res.status(200).send(database)
+})
 
 
 app.listen(5000, () => {
