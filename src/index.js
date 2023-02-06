df3ed0ca091ade5560bbc03146d64570186825ba
Thiago Mata <taiagomata@gmail.com>
2023-02-05 22:44:06 -0300

    Created PUT route

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const database = []
 
 app.use(express.json())
 
+// Adicionar Tasks
 app.post("/tasks", (req, res) => {
     const { title, description, } = req.body
     const date = new Date()
@@ -21,14 +22,43 @@ app.post("/tasks", (req, res) => {
 
     database.push(data)
 
-    console.log(database)
     return res.status(200).send("Tarefa Registrada!")
 
 })
 
+// Listar todas as Tasks
 app.get("/tasks", (req, res) => {
     return res.status(200).send(database)
 })
+
+app.put("/tasks/:id", (req, res) => {
+    const { id } = req.params
+    const { title, description } = req.body
+
+    const tasks = database.find(data => {
+        return data.id === id
+    })
+
+    console.log(title, description, tasks)
+
+
+    if(!tasks){
+        return res.status(404).send("Usuário não encontrado")
+    }
+
+    const newTask = database.map(data => {
+        if(data.id === id){
+            data.title = title
+            data.description = description
+        }
+        return data
+    }) 
+
+    res.status(200).send("Usuário alterado.")
+    
+})
+
+
 
 
 app.listen(5000, () => {
